1f4c62c1eaf8303e0d13197d27d8052289a1c70c
Victorpraiz <ivictorpraiz@gmail.com>
2023-02-15 22:54:26 +0100

    Responsive design

--- LandingMoove/src/App.jsx
+++ LandingMoove/src/App.jsx
@@ -1,24 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import LandingBody from './components/LandingBody'
-import Hero from './components/Hero'
-import Footer from './components/Footer'
+import { useState } from "react";
+import "./App.css";
+import LandingBody from "./components/LandingBody";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <Hero />
       <LandingBody />
       <Footer />
-    <div >
-     
+      <div></div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- LandingMoove/src/components/Hero.jsx
+++ LandingMoove/src/components/Hero.jsx
@@ -16,3 +16,4 @@ function Hero() {
 }
 
 export default Hero
+
