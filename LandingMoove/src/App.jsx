277fd971aeb544d8c1a69bcdda77f8d951be5fec
Victorpraiz <ivictorpraiz@gmail.com>
2023-02-16 22:51:58 +0100

    staged changes

--- LandingMoove/src/App.jsx
+++ LandingMoove/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="App ">
       <Hero />
       <LandingBody />
       <Footer />
--- LandingMoove/src/components/Navbar.jsx
+++ LandingMoove/src/components/Navbar.jsx
@@ -3,49 +3,59 @@ import { RxCaretDown } from "react-icons/rx";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
 
-function Navbar() {
+function Navbar({ handleOpen, open }) {
   const menu = [{ name: "Product" }, { name: "Company" }, { name: "Connect" }];
-  const [open, setOpen] = React.useState(true);
+  // const [open, setOpen] = React.useState(false);
   const icon = open ? (
     <GiHamburgerMenu
-      className="text-4xl absolute top-1 right-1 z-20"
+      className="lg:hidden text-4xl absolute top-12 right-10 z-20 hover:text-pink-200 duration-200"
       onClick={handleOpen}
     />
   ) : (
     <AiOutlineClose
-      className="text-4xl absolute top-1 right-1 z-20"
+      className="lg:hidden text-4xl absolute top-12 right-10 z-20  hover:text-pink-200 duration-200"
       onClick={handleOpen}
     />
   );
 
-  function handleOpen() {
-    setOpen((prev) => !prev);
-  }
+  // function handleOpen() {
+  //   setOpen((prev) => !prev);
+  // }
 
   return (
-    <div>
-      <nav className="lg:flex p-10 lg:px-40 justify-between  text-white">
-        <div className="lg:space-x-6 relative">
+    <div className="">
+      <nav
+        className={`lg:flex p-10 lg:px-40  text-white    lg:bg-transparent items-center lg:h-auto ${
+          !open && "h-screen bg-gradient-to-r  from-purple-500 to-pink-500"
+        } absolute lg:relative top-0 right-0 w-full `}
+      >
+        <div className="lg:mr-6 relative">
           <h1 className="text-3xl font-bold">Blogr</h1>
+        </div>
+        {icon}
 
-          {icon}
-          <ul
-            className={`${
-              open ? "right-[-40px]" : "right-[-400px]"
-            } lg:space-x-5 text-right top-[-40px] lg:inline absolute lg:static bg-black opacity-70 backdrop-blur-50 z-10 w-80 pt-40 pr-10 pb-10 rounded-br-lg rounded-bl-lg`}
-          >
+        <div
+          className={` flex justify-center lg:justify-between flex-col lg:flex-row w-full mt-5 lg:mt-0 ${
+            open ? "hidden " : "visible"
+          } `}
+        >
+          <ul className="lg:space-x-5 flex flex-col lg:flex-row items-center space-y-5 lg:space-y-0 pt-20 lg:pt-0">
             {menu.map((item) => (
-              <li className=" border-b-2 border-purple-300 lg:inline-block cursor-default lg:pt-0 py-5">
+              <li className="lg:border-none border-b-2  border-purple-300 cursor-default bg-pink-100 lg:hover:text-pink-200 lg:hover:bg-transparent hover:bg-pink-500 hover:bg-opacity-20 bg-opacity-20 lg:bg-transparent text-white text-center lg:text-left lg:text-inherit w-full lg:w-auto py-5 lg:py-0 pl-5 lg:pl-0 rounded-full duration-500">
                 <span>
                   {item.name} <RxCaretDown className="inline" />
                 </span>
               </li>
             ))}
-            <li className="py-5">Login</li>
-            <button className="rounded-full bg-white text-pink-500 p-2.5 mt-3 px-8 text-sm">
+          </ul>
+          <div className="flex flex-col lg:flex-row">
+            <button className="py-5 border-white lg:border-none border rounded-full mt-20 lg:mt-0 lg:ml-8 px-8 hover:bg-white lg:hover:bg-transparent hover:text-pink-500 lg:hover:text-inherit duration-500 transition-all lg:hover:text-pink-200">
+              Login
+            </button>
+            <button className="rounded-full bg-white text-pink-500 py-5  mt-3 lg:mt-0 lg:ml-2 px-8 text-sm  hover:bg-pink-100 lg:hover:bg-transparent hover:text-pink-500 lg:hover:text-inherit lg:hover-border lg:hover:border-white border duration-500 transition-all">
               Sign Up
             </button>
-          </ul>
+          </div>
         </div>
       </nav>
     </div>
